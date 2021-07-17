46b5ef4e736493993938937e84739b62a8757bcb
BuildTools <unconfigured@null.spigotmc.org>
2021-07-16 18:10:32 -0700

    Sketch in initial Qt application loop. Always copy python file, for slightly easier development. Copy amiga nethack font from nethack version 3.2.3

--- win/pyshim/nhvr.py
+++ win/pyshim/nhvr.py
@@ -3,35 +3,62 @@ from ctypes import c_char_p, c_int, POINTER
 import os
 import sys
 
-# Things to try:
-#  windll vs cdll
-#  debug vs release
-#  MT vs MD
+from PySide6 import QtCore, QtWidgets
 
-kernel32 = ctypes.WinDLL("kernel32", use_last_error=True)
-user32 = ctypes.WinDLL("user32", use_last_error=True)
-msvcrt = ctypes.WinDLL("msvcrt", use_last_error=True)
 
-# Load the nethack shared library DLL
-os.add_dll_directory(os.path.dirname(__file__))
-nethack_dll = ctypes.windll.LoadLibrary(
-    "NethackDLL.dll"
-    # "NethackVRDll.dll"
-)
-print(nethack_dll)
+class NethackWorker(QtCore.QObject):
+    finished = QtCore.Signal()
 
-# Expose the nethack main(...) function
-print(nethack_dll.main)
-nethack_dll.main.restype = c_int
-nethack_dll.main.argtypes = c_int, POINTER(c_char_p)  # argc, argv
+    def __init__(self, thread: QtCore.QThread):
+        super().__init__()
+        self.moveToThread(thread)
+        thread.started.connect(self.run)
+        self.finished.connect(thread.quit)
+        self.finished.connect(self.deleteLater)
 
-# Run the main(...) function
-print(sys.argv)
-argc = len(sys.argv)
-argv = [s.encode() for s in sys.argv]
-argv = (c_char_p * argc)(*argv)
-print(argv)
+    def run(self):
+        # Load the nethack shared library DLL
+        os.add_dll_directory(os.path.dirname(__file__))
+        nethack_dll = ctypes.windll.LoadLibrary(
+            "NethackDLL.dll"
+        )
+        print(nethack_dll)
 
-# Note I can only see the tty output by setting pycharm "Emulate terminal in output console"
-result = nethack_dll.main(argc, argv)
-print(result)
+        # Expose the nethack main(...) function
+        print(nethack_dll.main)
+        nethack_dll.main.restype = c_int
+        nethack_dll.main.argtypes = c_int, POINTER(c_char_p)  # argc, argv
+
+        # Prepare argc and argv arguments for the nethack main() function
+        argc = len(sys.argv)
+        argv = [s.encode() for s in sys.argv]
+        argv = (c_char_p * argc)(*argv)
+
+        # Run the main(...) function
+        # Note I can only see the tty output by setting pycharm "Emulate terminal in output console"
+        result = nethack_dll.main(argc, argv)
+        print(result)
+        self.finished.emit()
+
+
+# sketch in Qt event loop
+class NethackVrApp(QtWidgets.QApplication):
+    def __init__(self):
+        super().__init__(sys.argv)
+        self.nethack_thread = QtCore.QThread()
+        self.nethack_worker = NethackWorker(self.nethack_thread)
+
+    def run(self):
+        self.run_nethack()
+        label = QtWidgets.QLabel("Hello World")
+        label.show()
+        result = self.exec()
+        sys.exit(result)
+
+    def run_nethack(self):
+        self.nethack_thread.start()
+
+
+if __name__ == "__main__":
+    app = NethackVrApp()
+    app.run()
